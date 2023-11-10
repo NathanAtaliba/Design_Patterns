1f1f95c1f5668c725774f76b77b10d38d0fb5b7e
Nathan Santos Ataliba <nathan.s@inatel.br>
2023-11-10 12:39:33 -0300

    files deleted

--- principal.js
+++
@@ -1,8 +0,0 @@
-import ContadorPalavrasObserver from './contadores/contadorPalavrasObserver.js'
-
-const contadorPalavrasObserver = new ContadorPalavrasObserver();
-contadorPalavrasObserver.contar("Frase Teste um dois tres D gegdfgewrgewfgsd");
-  
-console.log("Total de palavras:", contadorPalavrasObserver.contadorPalavras.total);
-console.log("Total de palavras com quantidades pares de caracteres:", contadorPalavrasObserver.contadorPalavrasPares.total);
-console.log("Total de palavras começadas com maiúsculas:", contadorPalavrasObserver.contadorPalavrasComecadasComMaiusculas.total);
